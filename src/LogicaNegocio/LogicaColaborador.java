a6db540bca3a0dc2f28d4817d15e81e0868975c2
sques <sques@LAPTOP-SG08QF7A>
2024-04-07 16:58:10 -0600

    Calculo de salario neto

--- src/LogicaNegocio/LogicaColaborador.java
+++ src/LogicaNegocio/LogicaColaborador.java
@@ -9,12 +9,13 @@ public class LogicaColaborador {
      private static double totalSalarioBase;
      private static double totalHorasExtrasDobles;
      private static double totalHorasExtrasNormales;
-     private static double salarioSinDeducciones =10000000;
+     private static double salarioSinDeducciones= 10000000;
      private static double salarioSinDeduccionesDeCcssAlPatrono;
      private static double deduccionesCcssColaborador;
      private static double impuestosSobreRenta;
+     private static double salarioNeto;
      
-     
+  /* seccionpara pruenas  
     public static void main (String [] args){
         Colaborador colaborador = new Colaborador();
        // calculoHorasExtrasNormales(colaborador);
@@ -24,74 +25,82 @@ public class LogicaColaborador {
       //  salarioSinDeduccionesDeCcssAlPatrono();
         deduccionesCcssColaborador();
         impuestosSobreRenta();
-    }
+        calculoSalarioNeto();
+    }*/
     
     public static  double calculoHorasExtrasNormales(Colaborador objColaborador){
-       objColaborador.setExtrasCorrientes(15.0); 
-       objColaborador.setSalarioHora(15000.0);
+      // objColaborador.setExtrasCorrientes(15.0); 
+      // objColaborador.setSalarioHora(15000.0);
        totalHorasExtrasNormales = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora()*1.5;
-       System.out.println(totalHorasExtrasNormales);
+      // System.out.println(totalHorasExtrasNormales);
        return totalHorasExtrasNormales;
     }
     
      public static  double calculoHorasExtrasDobles(Colaborador objColaborador){
-       objColaborador.setExtrasDobles(15.0);
-       objColaborador.setSalarioHora(15000.0);
+      // objColaborador.setExtrasDobles(15.0);
+      // objColaborador.setSalarioHora(15000.0);
        totalHorasExtrasDobles = objColaborador.getExtrasDobles()*objColaborador.getSalarioHora()*2;
-       System.out.println(totalHorasExtrasDobles);
+      // System.out.println(totalHorasExtrasDobles);
        return totalHorasExtrasDobles;
     }
      
      public static  double calculoSalarioBase(Colaborador objColaborador){
-       objColaborador.setHorasTrabajadas(300.0);
-       objColaborador.setSalarioHora(15000.0);
+      // objColaborador.setHorasTrabajadas(300.0);
+      // objColaborador.setSalarioHora(15000.0);
        totalSalarioBase = objColaborador.getHorasTrabajadas()*objColaborador.getSalarioHora();
-       System.out.println(totalSalarioBase);
+      // System.out.println(totalSalarioBase);
        return totalSalarioBase;
     }
      
     public static double salarioSinDeducciones(){
           salarioSinDeducciones =  totalSalarioBase + totalHorasExtrasDobles + totalHorasExtrasNormales;
-          System.out.println(salarioSinDeducciones);
+        //  System.out.println(salarioSinDeducciones);
           return salarioSinDeducciones;
     }
     
     public static double salarioSinDeduccionesDeCcssAlPatrono(){
         salarioSinDeduccionesDeCcssAlPatrono = (100*salarioSinDeducciones)/73.33;
-        System.out.println(salarioSinDeduccionesDeCcssAlPatrono);
+       // System.out.println(salarioSinDeduccionesDeCcssAlPatrono);
         return salarioSinDeduccionesDeCcssAlPatrono;
     }
     
     public static double deduccionesCcssColaborador(){
         deduccionesCcssColaborador = salarioSinDeducciones *0.1067;
-        System.out.println(deduccionesCcssColaborador);
+        //System.out.println(deduccionesCcssColaborador);
         return deduccionesCcssColaborador;
     }
     
     public static double impuestosSobreRenta(){
      if(salarioSinDeducciones > 929000 && salarioSinDeducciones <1363000){        
          impuestosSobreRenta = (salarioSinDeducciones-929000)* 0.10; 
-          System.out.println(impuestosSobreRenta);
+        //  System.out.println(impuestosSobreRenta);
          return impuestosSobreRenta;
     } else if(salarioSinDeducciones > 1363000 && salarioSinDeducciones <2392000){
          impuestosSobreRenta = (salarioSinDeducciones-1363000) * 0.15 + 43400; 
-          System.out.println(impuestosSobreRenta);
+         // System.out.println(impuestosSobreRenta);
          return impuestosSobreRenta;
     } else if (salarioSinDeducciones > 2392000 && salarioSinDeducciones <4783000){
          impuestosSobreRenta = (salarioSinDeducciones-2392000) * 0.20 + 197750;  
-          System.out.println(impuestosSobreRenta);
+         // System.out.println(impuestosSobreRenta);
          return impuestosSobreRenta;
     }else if(salarioSinDeducciones > 4783000){
          impuestosSobreRenta = (salarioSinDeducciones-4783000) * 0.25 + 675950; 
-          System.out.println(impuestosSobreRenta);
+        //  System.out.println(impuestosSobreRenta);
          return impuestosSobreRenta;
     }
     else {
         impuestosSobreRenta = 0.0;
-        System.out.println(impuestosSobreRenta);
+       // System.out.println(impuestosSobreRenta);
     }
      return impuestosSobreRenta;
     }
+    
+    public static double calculoSalarioNeto(){
+        salarioNeto = salarioSinDeducciones - impuestosSobreRenta - deduccionesCcssColaborador;
+       // System.out.println(salarioNeto);
+        return salarioNeto;
+    }
+            
 
     public static double getTotalSalarioBase() {
         return totalSalarioBase;
