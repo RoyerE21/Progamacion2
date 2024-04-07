cb480443ba07759995055c42b66bb85b3c298f27
sques <sques@LAPTOP-SG08QF7A>
2024-04-07 14:33:27 -0600

    Calculos para salario con deducciones de caja en ambas partes

--- src/LogicaNegocio/LogicaColaborador.java
+++ src/LogicaNegocio/LogicaColaborador.java
@@ -10,12 +10,16 @@ public class LogicaColaborador {
      private static double totalHorasExtrasDobles;
      private static double totalHorasExtrasNormales;
      private static double salarioSinDeducciones;
+     private static double salarioSinDeduccionesDeCcssAlPatrono;
+     private static double deduccionesCcssColaborador;
     public static void main (String [] args){
         Colaborador colaborador = new Colaborador();
         calculoHorasExtrasNormales(colaborador);
         calculoHorasExtrasDobles(colaborador);
         calculoSalarioBase(colaborador);
         salarioSinDeducciones();
+        salarioSinDeduccionesDeCcssAlPatrono();
+        deduccionesCcssColaborador();
     }
     
     public static  double calculoHorasExtrasNormales(Colaborador objColaborador){
@@ -48,6 +52,17 @@ public class LogicaColaborador {
           return salarioSinDeducciones;
     }
     
+    public static double salarioSinDeduccionesDeCcssAlPatrono(){
+        salarioSinDeduccionesDeCcssAlPatrono = (100*salarioSinDeducciones)/73.33;
+        System.out.println(salarioSinDeduccionesDeCcssAlPatrono);
+        return salarioSinDeduccionesDeCcssAlPatrono;
+    }
+    
+    public static double deduccionesCcssColaborador(){
+        deduccionesCcssColaborador = salarioSinDeducciones *0.1067;
+        System.out.println(deduccionesCcssColaborador);
+        return deduccionesCcssColaborador;
+    }
 
     public static double getTotalSalarioBase() {
         return totalSalarioBase;
@@ -63,6 +78,10 @@ public class LogicaColaborador {
 
     public static double getSalarioSinDeducciones() {
         return salarioSinDeducciones;
+    }
+
+    public static double getSalarioSinDeduccionesDeCcssAlPatrono() {
+        return salarioSinDeduccionesDeCcssAlPatrono;
     }
     
     
