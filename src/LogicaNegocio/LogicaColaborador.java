9eb16890415bacb40718864ed033f9fd9a602a1a
sques <sques@LAPTOP-SG08QF7A>
2024-04-07 13:35:33 -0600

    Calculos para salario sin deducciones al colaborador

--- src/LogicaNegocio/LogicaColaborador.java
+++ src/LogicaNegocio/LogicaColaborador.java
@@ -6,35 +6,65 @@ import Entidades.Colaborador;
 
 public class LogicaColaborador {
     
+     private static double totalSalarioBase;
+     private static double totalHorasExtrasDobles;
+     private static double totalHorasExtrasNormales;
+     private static double salarioSinDeducciones;
     public static void main (String [] args){
         Colaborador colaborador = new Colaborador();
-        //calculoHorasExtrasNormales(colaborador);
+        calculoHorasExtrasNormales(colaborador);
         calculoHorasExtrasDobles(colaborador);
         calculoSalarioBase(colaborador);
+        salarioSinDeducciones();
     }
     
     public static  double calculoHorasExtrasNormales(Colaborador objColaborador){
        objColaborador.setExtrasCorrientes(15.0);
        objColaborador.setSalarioHora(1500.0);
-       double totalHorasExtrasNormales = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora()*1.5;
+       totalHorasExtrasNormales = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora()*1.5;
        System.out.println(totalHorasExtrasNormales);
        return totalHorasExtrasNormales;
     }
     
      public static  double calculoHorasExtrasDobles(Colaborador objColaborador){
-       objColaborador.setExtrasCorrientes(15.0);
+       objColaborador.setExtrasDobles(15.0);
        objColaborador.setSalarioHora(1500.0);
-       double totalHorasExtrasDobles = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora()*2;
+       totalHorasExtrasDobles = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora()*2;
        System.out.println(totalHorasExtrasDobles);
        return totalHorasExtrasDobles;
     }
      
      public static  double calculoSalarioBase(Colaborador objColaborador){
-       objColaborador.setExtrasCorrientes(15.0);
+       objColaborador.setHorasTrabajadas(15.0);
        objColaborador.setSalarioHora(1500.0);
-       double totalSalarioBase = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora();
+       totalSalarioBase = objColaborador.getExtrasCorrientes()*objColaborador.getSalarioHora();
        System.out.println(totalSalarioBase);
        return totalSalarioBase;
     }
+     
+    public static double salarioSinDeducciones(){
+          salarioSinDeducciones =  totalSalarioBase + totalHorasExtrasDobles + totalHorasExtrasNormales;
+          System.out.println(salarioSinDeducciones);
+          return salarioSinDeducciones;
+    }
+    
+
+    public static double getTotalSalarioBase() {
+        return totalSalarioBase;
+    }
+
+    public static double getTotalHorasExtrasDobles() {
+        return totalHorasExtrasDobles;
+    }
+
+    public static double getTotalHorasExtrasNormales() {
+        return totalHorasExtrasNormales;
+    }
+
+    public static double getSalarioSinDeducciones() {
+        return salarioSinDeducciones;
+    }
+    
+    
     
 }
