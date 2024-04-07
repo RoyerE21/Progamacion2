25f5ebaca9f16c88653dfb4a3bda42d81f940129
sques <sques@LAPTOP-SG08QF7A>
2024-04-07 12:18:28 -0600

    Metodos getters/setters en colaborador

--- src/Entidades/Colaborador.java
+++ src/Entidades/Colaborador.java
@@ -8,10 +8,88 @@ package Entidades;
 public class Colaborador {
     
     
-    private double salarioHora, horasTrabajadas, extrasSencillas, extrasDobles;
+    private double salarioHora, horasTrabajadas, extrasCorrientes, extrasDobles;
     private String nombre, primerApellido, segundoApellido, identificacion, correoElectronico, ocupacion;
 
-    private int fhuvb;
-    
-    private int hola; 
+    public double getSalarioHora() {
+        return salarioHora;
+    }
+
+    public void setSalarioHora(double salarioHora) {
+        this.salarioHora = salarioHora;
+    }
+
+    public double getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(double horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public double getExtrasCorrientes() {
+        return extrasCorrientes;
+    }
+
+    public void setExtrasCorrientes(double extrasCorrientes) {
+        this.extrasCorrientes = extrasCorrientes;
+    }
+
+    public double getExtrasDobles() {
+        return extrasDobles;
+    }
+
+    public void setExtrasDobles(double extrasDobles) {
+        this.extrasDobles = extrasDobles;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+
 }
